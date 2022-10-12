c219348fd077b01eef93f9095a6e05a1c439bccd
STBrinkmann <bastibrinkmann94@gmail.com>
2022-10-13 01:00:53 +0200

    Updated libraries

--- workflow/00_setup.R
+++ workflow/00_setup.R
@@ -1,6 +1,22 @@
 packages_needed <- c(
   "magrittr", "terra", "sf", "dplyr", "kableExtra", "knitr", "ggplot2",
-  "classInt", "spatLac"
+  "classInt", "spatLac", "sfheaders", "GVI", "osmdata", "osrm", "pbmcapply",
+  "DRIGLUCoSE", "bartMachine", "tidymodels", "mapview", "psych", "tidyverse",
+  "ggpubr", "foreach", "Rcpp", "RcppProgress", "doParallel"
 )
 
-packages_needed %in% installed.packages()[,1]
+for(i in which(!packages_needed %in% installed.packages()[,1])){
+  this_package <- packages_needed[i]
+  
+  if(!this_package %in% installed.packages()[,1]){
+    if(this_package == "DRIGLUCoSE"){
+      remotes::install_git("https://github.com/STBrinkmann/DRIGLUCoSE")
+    } else if(this_package == "spatLac"){
+      devtools::install_github("STBrinkmann/spatLac")
+    } else if(this_package == "GVI"){
+       remotes::install_git("https://github.com/STBrinkmann/GVI")
+    } else {
+      install.packages(this_package)
+    }
+  }  
+}
